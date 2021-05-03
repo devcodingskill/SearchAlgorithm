2be3a922092945d9db144442639e479b4a756f59
Tan <decodingskill@gmail.com>
2021-05-03 18:34:27 +1000

    first file add

---
+++ SimpleSearch/SimpleSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleSearch
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
